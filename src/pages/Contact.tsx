5d90e707246dd89614ee869871498ef26f97436a
YaqubRahman <YaqubRahman999@gmail.com>
2025-05-07 07:57:28 +0100

    fix: links in contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,35 +1,59 @@
-import '../App.css'
-
+import "../App.css";
 
 function Contact() {
-
   return (
     <>
-    <div className="container">
-      <h1><br></br>Contact<br></br><br></br> </h1>
-      </div>
-      
-      <div className="contacts-title ">
-      <h1><br></br>Phone: +44 7500 464211<br></br><br></br> </h1>
+      <div className="container">
+        <h1>
+          <br></br>Contact<br></br>
+          <br></br>{" "}
+        </h1>
       </div>
 
       <div className="contacts-title ">
-      <h1><br></br>Email: YaqubRahman999@gmail.com<br></br><br></br> </h1>
+        <h1>
+          <br></br>Phone: +44 7500 464211<br></br>
+          <br></br>{" "}
+        </h1>
+      </div>
+
+      <div className="contacts-title ">
+        <h1>
+          <br></br>Email: YaqubRahman999@gmail.com<br></br>
+          <br></br>{" "}
+        </h1>
       </div>
       <div className="contacts-title">
-    <h1><br />Github:<br /><a href="https://github.com/YaqubRahman">https://github.com/YaqubRahman</a><br /><br /></h1>
-    </div>
-
-      <div className="contacts-title ">
-      <h1><br />Linkedin:<br /><a href="linkedin.com/in/yaqub-r-628524298">linkedin.com/in/yaqub-r-628524298</a><br /><br /></h1>
+        <h1>
+          <br />
+          Github:
+          <br />
+          <a href="https://github.com/YaqubRahman">
+            https://github.com/YaqubRahman
+          </a>
+          <br />
+          <br />
+        </h1>
       </div>
 
-        <div className='hero-subtitle'>
-        <p></p>
-        </div>
+      <div className="contacts-title ">
+        <h1>
+          <br />
+          Linkedin:
+          <br />
+          <a href="https://www.linkedin.com/in/yaqub-r">
+            https://www.linkedin.com/in/yaqub-r
+          </a>
+          <br />
+          <br />
+        </h1>
+      </div>
 
+      <div className="hero-subtitle">
+        <p></p>
+      </div>
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
