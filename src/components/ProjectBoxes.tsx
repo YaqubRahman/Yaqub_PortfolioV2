5561bbdd70f3fdf7085d767b99a6c7232ac9e99b
YaqubRahman <YaqubRahman999@gmail.com>
2025-05-07 07:40:40 +0100

    fix: colour of light theme background and project text colour

--- src/components/ProjectBoxes.tsx
+++ src/components/ProjectBoxes.tsx
@@ -1,84 +1,99 @@
-import PlaneMainMenu from '../assets/projectassets/planemainmenu.png'
-import KNearest from '../assets/projectassets/K-Nearest.png'
-import Fractal from '../assets/projectassets/FractalPlotting.png'
-import DecisionTree from '../assets/projectassets/DecisionTree.png'
-import ShoppingList from '../assets/projectassets/ShoppingList.png'
-import WeatherApp from '../assets/projectassets/WeatherApp.png'
-import SwiftdIn from '../assets/projectassets/SwiftdIn.png'
-import PrayerApp from '../assets/projectassets/PrayerApp.png'
-import Portfolio from '../assets/projectassets/PortfolioProjectImage.png'
+import PlaneMainMenu from "../assets/projectassets/planemainmenu.png";
+import KNearest from "../assets/projectassets/K-Nearest.png";
+import Fractal from "../assets/projectassets/FractalPlotting.png";
+import DecisionTree from "../assets/projectassets/DecisionTree.png";
+import ShoppingList from "../assets/projectassets/ShoppingList.png";
+import WeatherApp from "../assets/projectassets/WeatherApp.png";
+import SwiftdIn from "../assets/projectassets/SwiftdIn.png";
+import PrayerApp from "../assets/projectassets/PrayerApp.png";
+import Portfolio from "../assets/projectassets/PortfolioProjectImage.png";
+import UniStreetFighter from "../assets/projectassets/UniStreetFighter.png";
 
-function ProjectBoxes(){
-    return(
-        <div>
-              <p className="read-the-docs">
-        Still work in progress!
-      </p>
-            <div className="boxflex">
-                <div className="box">
-                    <h1 className='title'>YR13 NEA Game</h1>
-                    <div className="pythonlogo">
-                        <p>A python game which uses<br></br>
-                         pygame. Developed using an agile<br></br>
-                         methodology and uses the<br></br>
-                          principles of SUVAT from physics.</p>
-                    </div>
-                    <img src={PlaneMainMenu} className="image" alt="plainmainmenu" />
-                </div>
-                
-                <div className="box">
-                    <h1 className='title'>K-Nearest Neighbours ML</h1>
-                    <img src={KNearest} className="image" alt="knearest" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>Fractal Plotting</h1>
-                    <img src={Fractal} className="image" alt="fractal" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>SwiftdIn Full Stack App</h1>
-                    <div className="pythonlogo">
-                        <p>A full stack web application which uses React, JavaScript and MongoDB. SwiftdIn
-                          is a LinedIn<br></br>
-                           like clone.</p>
-
-                    </div>
-                    <img src={SwiftdIn} className="image" alt="plainmainmenu" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>Decision Tree ML</h1>
-                    <img src={DecisionTree} className="image" alt="decisiontree" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>Prayer App</h1>
-                    <div className="pythonlogo">
-                        <p>A Kotlin App, collaborated with a<br></br> 
-                         team, using API's to fetch prayer times.</p>
-                    </div>
-                    <img src={PrayerApp} className="image" alt="prayerapp" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>Shopping List App</h1>
-                    <img src={ShoppingList} className="image" alt="shoppinglist" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>Weather Web App</h1>
-                    <img src={WeatherApp} className="image" alt="weatherapp" />
-                </div>
-
-                <div className="box">
-                    <h1 className='title'>This website :)</h1>
-                    <img src={Portfolio} className="image" alt="portfolioimage" />
-                </div>
-
-            </div>
+function ProjectBoxes() {
+  return (
+    <div>
+      <p className="read-the-docs">Still work in progress!</p>
+      <div className="boxflex">
+        <div className="box">
+          <h1 className="title">YR13 NEA Game</h1>
+          <div className="pythonlogo">
+            <p>
+              A python game which uses<br></br>
+              pygame. Developed using an agile<br></br>
+              methodology and uses the<br></br>
+              principles of SUVAT from physics.
+            </p>
+          </div>
+          <img src={PlaneMainMenu} className="image" alt="plainmainmenu" />
         </div>
-    )
+
+        <div className="box">
+          <h1 className="title">K-Nearest Neighbours ML</h1>
+          <img src={KNearest} className="image" alt="knearest" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">Fractal Plotting</h1>
+          <img src={Fractal} className="image" alt="fractal" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">SwiftdIn Full Stack App</h1>
+          <div className="pythonlogo">
+            <p>
+              A full stack web application which uses React, JavaScript and
+              MongoDB. SwiftdIn is a LinedIn<br></br>
+              like clone.
+            </p>
+          </div>
+          <img src={SwiftdIn} className="image" alt="plainmainmenu" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">Decision Tree ML</h1>
+          <img src={DecisionTree} className="image" alt="decisiontree" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">Prayer App</h1>
+          <div className="pythonlogo">
+            <p>
+              A Kotlin App, collaborated with a<br></br>
+              team, using API's to fetch prayer times.
+            </p>
+          </div>
+          <img src={PrayerApp} className="image" alt="prayerapp" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">Shopping List App</h1>
+          <img src={ShoppingList} className="image" alt="shoppinglist" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">University Street Fighter Game</h1>
+          <div className="pythonlogo">
+            <p>A Java game that uses City Engine</p>
+          </div>
+          <img
+            src={UniStreetFighter}
+            className="image"
+            alt="unistreetfighter"
+          />
+        </div>
+
+        <div className="box">
+          <h1 className="title">Weather Web App</h1>
+          <img src={WeatherApp} className="image" alt="weatherapp" />
+        </div>
+
+        <div className="box">
+          <h1 className="title">This website :)</h1>
+          <img src={Portfolio} className="image" alt="portfolioimage" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectBoxes;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,65 @@
-import Images from '../components/Images'
-import yaqub from '../assets/yaqubpfp.jpg'
-import biannual from '../assets/biannual.jpeg'
-import '../App.css'
-import soukaiLogo from '../assets/soukAI pfp2.png'
-
-
-
+import Images from "../components/Images";
+import yaqub from "../assets/yaqubpfp.jpg";
+import biannual from "../assets/biannual.jpeg";
+import "../App.css";
+import soukaiLogo from "../assets/soukAI pfp2.png";
 
 function Home() {
   return (
     <>
-    <div className="container ">
-    <div className='typewriter'>
-      <h1>Yaqub Rahman</h1>
-    <div className="sub-title">
-      <h2>Software Engineer at SoukAI</h2>
-    </div>
+      <div className="container ">
+        <div className="typewriter">
+          <h1>Yaqub Rahman</h1>
+          <div className="sub-title">
+            <h2>Software Engineer at SoukAI</h2>
+          </div>
 
-    <a href="https://www.linkedin.com/company/souk-ai/" target="_blank">
-        <img src={soukaiLogo} className="soukai scroll-content scroll-container" alt="soukai logo" />
-        </a>
-
-
-    </div>
-      <div className="content">
-      <img src={yaqub} className="yaqub" alt="yaqub pfp" />
+          <a href="https://souk-ai.co.uk/" target="_blank">
+            <img
+              src={soukaiLogo}
+              className="soukai scroll-content scroll-container"
+              alt="soukai logo"
+            />
+          </a>
+        </div>
+        <div className="content">
+          <img src={yaqub} className="yaqub" alt="yaqub pfp" />
+        </div>
       </div>
-      </div>
-      <Images/>
+      <Images />
       <div className="card">
-        <div className='hero-title '>
-        <p>Hi I am a first year Computer Science undergraduate!</p>
+        <div className="hero-title ">
+          <p>Hi I am a first year Computer Science undergraduate!</p>
         </div>
 
+        <div className="herowords-title herotextcontainer">
+          <p>
+            <br></br>"Disciplined"<br></br>
+            <br></br>"Hardworking"<br></br>
+            <br></br>"Composed"<br></br>
+            <br></br>"Determined"
+          </p>
 
-        <div className='herowords-title herotextcontainer'>
-        <p><br></br>"Disciplined"<br></br><br></br>"Hardworking"<br></br><br></br>"Composed"<br></br><br></br>"Determined"</p>
-      
-
-        <div className="hero-image herotextcontent ">
-        <a href="https://www.johnlyon.org/ccf-biennial-inspection/" target="_blank">
-        <img src={biannual} className="biannual" alt="biannual" />
-        </a>
+          <div className="hero-image herotextcontent ">
+            <a
+              href="https://lyonian-association.org/news/events/229/229-OLs-return-for-the-CCF-Biennial-Inspection"
+              target="_blank"
+            >
+              <img src={biannual} className="biannual" alt="biannual" />
+            </a>
+          </div>
         </div>
-        </div>
 
-        <div className='hero-subtitle'>
-        <p></p>
+        <div className="hero-subtitle">
+          <p></p>
         </div>
 
         <p className="read-the-docs">
-        Click on the logos and images to learn more
-      </p>
+          Click on the logos and images to learn more
+        </p>
       </div>
-
     </>
-  )
+  );
 }
 
-export default Home
-
-
+export default Home;
