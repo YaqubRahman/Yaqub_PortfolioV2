1991640c796a1b1963c5fcec3224941b66ad4ef4
YaqubRahman <YaqubRahman999@gmail.com>
2025-05-07 07:49:10 +0100

    removed: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,27 @@
-import { Link } from 'react-router-dom';
-import './Navbar.css'
-import search_icon_light from '../assets/search-w.png'
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import search_icon_light from "../assets/search-w.png";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <h1></h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/Experience">Experience</Link>
-                </li>
-                <li>
-                    <Link to="/Projects">Projects</Link>
-                </li>
-                <li>
-                    <Link to="/Contact">Contact</Link>
-                </li>
-            </ul>
-            <div className='search-box'>
-                <input type="text" placeholder='Search'/>
-                <img src={search_icon_light} alt=""/>
-            </div>
-            <img src="" alt="" className='toggle-icon'/>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <h1></h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Experience">Experience</Link>
+        </li>
+        <li>
+          <Link to="/Projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
