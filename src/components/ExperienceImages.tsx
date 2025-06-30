a85dcd4ca8347198093c45f95fd0a5528fcc7b22
YaqubRahman <YaqubRahman999@gmail.com>
2025-06-30 11:17:03 +0100

    feat: added more experiences

--- src/components/ExperienceImages.tsx
+++ src/components/ExperienceImages.tsx
@@ -1,174 +1,190 @@
-import Encode from '../assets/experienceassets/EncodeHackathon.jpg'
-import CCF from '../assets/experienceassets/CCFAward.jpg'
-import SoukAI from '../assets/experienceassets/SoukAI.jpg'
-import Imperial from '../assets/experienceassets/ImperialHackathon.jpg'
-import Headstudent from '../assets/experienceassets/headstudent.jpg'
-import Awsevent from '../assets/experienceassets/awsevent.jpg'
-import Robotics from '../assets/experienceassets/Robotics.jpg'
-import KCL from '../assets/experienceassets/KCLHackathon.jpg'
-import Buildathon from '../assets/experienceassets/Buildathon.jpg'
-import './Experience.css'
+import Encode from "../assets/experienceassets/EncodeHackathon.jpg";
+import CCF from "../assets/experienceassets/CCFAward.jpg";
+import Imperial from "../assets/experienceassets/ImperialHackathon.jpg";
+import Headstudent from "../assets/experienceassets/headstudent.jpg";
+import Awsevent from "../assets/experienceassets/awsevent.jpg";
+import Robotics from "../assets/experienceassets/Robotics.jpg";
+import KCL from "../assets/experienceassets/KCLHackathon.jpg";
+import Buildathon from "../assets/experienceassets/Buildathon.jpg";
+import MTF from "../assets/experienceassets/MTF.jpg";
+import SoukAITeam from "../assets/experienceassets/SoukAITeam.jpg";
+import RedwoodFounders from "../assets/experienceassets/RedwoodFounders.jpg";
+import Cur8 from "../assets/experienceassets/Cur8Capital.png";
+import "./Experience.css";
 
-
-function ExperienceImages(){
-    return(
-        <div>
-
-
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p><span className="bronze-text">3rd Place</span> Cambridge Joint ISoc Buildathon</p>
+function ExperienceImages() {
+  return (
+    <div>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>Redwood Founders Accelerator Programme</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>Feb 2025</p>
+          <p>June 2025</p>
         </div>
+      </div>
 
-        </div>
+      <img src={RedwoodFounders} className="encodeexperience" alt="redwood" />
 
-
-        <img src={Buildathon} className="encodeexperience" alt="buildathon" />
-
-        <div className="card">
-            <div className='experiencehero-title'>
-                <p><span className="silver-text">2nd Place</span> KCL Enactus Hackathon</p>
-                </div>
-                <div className="experiencehero-subtitle">
-                    <p>Feb 2025</p>
-                    </div>
-        </div>
-
-
-        <img src={KCL} className="encodeexperience" alt="kcl" />
-
-
-
-
-
-
-
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p><span className="gold-text">1st Place</span> Imperial Hackathon Winner Summer '24</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p> Cur8 Capital SWE Internship</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>June 2024</p>
+          <p>June-July 2025</p>
         </div>
-        </div>
+      </div>
 
-        <img src={Imperial} className="encodeexperience" alt="imperial" />
+      <img src={Cur8} className="encodeexperience" alt="cur8" />
 
-
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>CCF Staff Sergeant</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>Muslim Tech Fest 2025</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>2019-2024</p>
+          <p>June 2025</p>
         </div>
+      </div>
 
-        </div>
+      <img src={MTF} className="encodeexperience" alt="mtf" />
 
-
-        <img src={CCF} className="encodeexperience" alt="ccf" />
-
-
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p><span className="silver-text">2nd Place</span> Robotics End Of Line Competition</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>
+            <span className="bronze-text">3rd Place</span> Cambridge Joint ISoc
+            Buildathon
+          </p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>Sep - Dec 2024</p>
+          <p>Feb 2025</p>
         </div>
+      </div>
 
-        </div>
+      <img src={Buildathon} className="encodeexperience" alt="buildathon" />
 
-
-        <img src={Robotics} className="encodeexperience" alt="robotics" />
-
-
-
-
-
-
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>Encode Hackathon/Conference</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>
+            <span className="silver-text">2nd Place</span> KCL Enactus Hackathon
+          </p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>Oct 2024</p>
+          <p>Feb 2025</p>
         </div>
-        </div>
+      </div>
 
-        <img src={Encode} className="encodeexperience" alt="encodeexperience" />
+      <img src={KCL} className="encodeexperience" alt="kcl" />
 
-
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>Souk AI Startup Software Engineer</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>
+            <span className="silver-text">2nd Place</span> Robotics End Of Line
+            Competition
+          </p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>Oct 2024-Present</p>
+          <p>Sep - Dec 2024</p>
         </div>
-        </div>
-            <img src={SoukAI} className="encodeexperience" alt="soukai" />
+      </div>
 
+      <img src={Robotics} className="encodeexperience" alt="robotics" />
 
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>AWS Immersion Day</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>Encode Hackathon/Conference</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>Oct 2024</p>
+          <p>Oct 2024</p>
         </div>
-        </div>
+      </div>
 
+      <img src={Encode} className="encodeexperience" alt="encodeexperience" />
 
-        <img src={Awsevent} className="encodeexperience" alt="awsevent" />
-        
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>Pinner High School Head Boy</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>Souk AI Startup Software Engineer</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>2023-2024</p>
+          <p>Oct 2024-Present</p>
         </div>
-        </div>
+      </div>
+      <img src={SoukAITeam} className="encodeexperience" alt="soukai" />
 
-        <img src={Headstudent} className="encodeexperience" alt="headstudent" />
-        
-        <div className="card">
-        <div className='experiencehero-title '>
-        <p>Scouts Leader</p>
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>AWS Immersion Day</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>2022-Present</p>
+          <p>Oct 2024</p>
         </div>
-        </div>
+      </div>
 
-        
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>TCS Cybersecurity Work Experience</p>
+      <img src={Awsevent} className="encodeexperience" alt="awsevent" />
+
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>
+            <span className="gold-text">1st Place</span> Imperial Hackathon
+            Winner Summer '24
+          </p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>June 2023</p>
+          <p>June 2024</p>
         </div>
-        </div>
-        
-        
-        <div className="card">
-        <div className='experiencehero-title'>
-        <p>DSTL Work Experience</p>
+      </div>
+
+      <img src={Imperial} className="encodeexperience" alt="imperial" />
+
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>CCF Staff Sergeant</p>
         </div>
         <div className="experiencehero-subtitle">
-            <p>Feb 2021</p>
+          <p>2019-2024</p>
         </div>
+      </div>
+
+      <img src={CCF} className="encodeexperience" alt="ccf" />
+
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>Pinner High School Head Boy</p>
         </div>
-
-
+        <div className="experiencehero-subtitle">
+          <p>2023-2024</p>
         </div>
-    )
+      </div>
 
+      <img src={Headstudent} className="encodeexperience" alt="headstudent" />
+
+      <div className="card">
+        <div className="experiencehero-title ">
+          <p>Scouts Leader</p>
+        </div>
+        <div className="experiencehero-subtitle">
+          <p>2022-Present</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>TCS Cybersecurity Work Experience</p>
+        </div>
+        <div className="experiencehero-subtitle">
+          <p>June 2023</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="experiencehero-title">
+          <p>DSTL Work Experience</p>
+        </div>
+        <div className="experiencehero-subtitle">
+          <p>Feb 2021</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ExperienceImages;
--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,53 +1,105 @@
-import stemmLogo from '../assets/stemm.jpg'
-import awsLogo from '../assets/aws.jpeg'
-import dstLogo from '../assets/dst.png' 
-import encodeLogo from '../assets/encode_club_logo.jpg' 
-import scoutsLogo from '../assets/scouts.png' 
-import ccfLogo from '../assets/ccf.jpg' 
-import phsLogo from '../assets/PHS.jpg'
-import tcsLogo from '../assets/tcs.jpg'
-import cityLogo from '../assets/city.png'
-import coinruleLogo from '../assets/coinrule.png'
+import stemmLogo from "../assets/stemm.jpg";
+import awsLogo from "../assets/aws.jpeg";
+import dstLogo from "../assets/dst.png";
+import encodeLogo from "../assets/encode_club_logo.jpg";
+import scoutsLogo from "../assets/scouts.png";
+import ccfLogo from "../assets/ccf.jpg";
+import phsLogo from "../assets/PHS.jpg";
+import tcsLogo from "../assets/tcs.jpg";
+import cityLogo from "../assets/city.png";
+import coinruleLogo from "../assets/coinrule.png";
+import cur8Logo from "../assets/Cur8.png";
 
-function Images(){
-    return(
-        <div>
-        <a href="https://stemmuslims.org/" target="_blank">
-        <img src={stemmLogo} className="stemmlogo scroll-content scroll-container" alt="stemm pfp" />
-        </a>
-        <a href="https://aws.amazon.com/" target="_blank">
-        <img src={awsLogo} className="aws scroll-content scroll-container" alt="aws logo" />
-        </a>
-        <a href="https://www.gov.uk/government/organisations/defence-science-and-technology-laboratory" target="_blank">
-        <img src={dstLogo} className="dst scroll-content scroll-container" alt="dst logo" />
-        </a>
-        <a href="https://www.encode.club/" target="_blank">
-        <img src={encodeLogo} className="encode scroll-content scroll-container" alt="encode logo" />
-        </a>
-        <a href="https://www.scouts.org.uk/" target="_blank">
-        <img src={scoutsLogo} className="scouts scroll-content scroll-container" alt="scouts logo" />
-        </a>
-        <a href="https://combinedcadetforce.org.uk/" target="_blank">
-        <img src={ccfLogo} className="ccf scroll-content scroll-container" alt="ccf logo" />
-        </a>
-        <a href="https://www.pinnerhighschool.org/" target="_blank">
-        <img src={phsLogo} className="phs scroll-content scroll-container" alt="phs logo" />
-        </a>
-        <a href="https://www.tcs.com/what-we-do/services/cybersecurity" target="_blank">
-        <img src={tcsLogo} className="tcs scroll-content scroll-container" alt="tcs logo" />
-        </a>
-        <a href="https://www.city.ac.uk/" target="_blank">
-        <img src={cityLogo} className="city scroll-content scroll-container" alt="city logo" />
-        </a>
+function Images() {
+  return (
+    <div>
+      <a href="https://stemmuslims.org/" target="_blank">
+        <img
+          src={stemmLogo}
+          className="stemmlogo scroll-content scroll-container"
+          alt="stemm pfp"
+        />
+      </a>
+      <a href="https://aws.amazon.com/" target="_blank">
+        <img
+          src={awsLogo}
+          className="aws scroll-content scroll-container"
+          alt="aws logo"
+        />
+      </a>
+      <a
+        href="https://www.gov.uk/government/organisations/defence-science-and-technology-laboratory"
+        target="_blank"
+      >
+        <img
+          src={dstLogo}
+          className="dst scroll-content scroll-container"
+          alt="dst logo"
+        />
+      </a>
+      <a href="https://www.encode.club/" target="_blank">
+        <img
+          src={encodeLogo}
+          className="encode scroll-content scroll-container"
+          alt="encode logo"
+        />
+      </a>
+      <a href="https://www.scouts.org.uk/" target="_blank">
+        <img
+          src={scoutsLogo}
+          className="scouts scroll-content scroll-container"
+          alt="scouts logo"
+        />
+      </a>
+      <a href="https://combinedcadetforce.org.uk/" target="_blank">
+        <img
+          src={ccfLogo}
+          className="ccf scroll-content scroll-container"
+          alt="ccf logo"
+        />
+      </a>
+      <a href="https://www.pinnerhighschool.org/" target="_blank">
+        <img
+          src={phsLogo}
+          className="phs scroll-content scroll-container"
+          alt="phs logo"
+        />
+      </a>
+      <a
+        href="https://www.tcs.com/what-we-do/services/cybersecurity"
+        target="_blank"
+      >
+        <img
+          src={tcsLogo}
+          className="tcs scroll-content scroll-container"
+          alt="tcs logo"
+        />
+      </a>
+      <a href="https://www.city.ac.uk/" target="_blank">
+        <img
+          src={cityLogo}
+          className="city scroll-content scroll-container"
+          alt="city logo"
+        />
+      </a>
 
-        <a href="https://coinrule.com/" target="_blank">
-        <img src={coinruleLogo} className="coinrule scroll-content scroll-container" alt="coinrule logo" />
-        </a>
+      <a href="https://coinrule.com/" target="_blank">
+        <img
+          src={coinruleLogo}
+          className="coinrule scroll-content scroll-container"
+          alt="coinrule logo"
+        />
+      </a>
 
-        
-        </div>
-    )
-
+      <a href="https://cur8.capital/" target="_blank">
+        <img
+          src={cur8Logo}
+          className="cur8 scroll-content scroll-container"
+          alt="cur8capital logo"
+        />
+      </a>
+    </div>
+  );
 }
 
 export default Images;
