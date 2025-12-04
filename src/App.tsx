c630afa5620c13f05f439210bbbf4520b9f1711b
Yaqub Rahman <yaqubrahman999@gmail.com>
2025-12-04 13:29:22 +0000

    feat: glow for mouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Navbar from './components/Navbar'
-import Experience from './pages/Experience';
-import Home from './pages/Home';
-import Contact from './pages/Contact'
-import Projects from './pages/Projects';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Experience from "./pages/Experience";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import { useEffect, useRef } from "react";
 
 export default function App() {
+  const glowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (glowRef.current) {
+        glowRef.current.style.left = `${e.clientX}px`;
+        glowRef.current.style.top = `${e.clientY}px`;
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => document.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <Router>
-      <div className='containerNav'>
-        <Navbar/>
+      <div ref={glowRef} className="glow"></div>
+      <div className="containerNav">
+        <Navbar />
       </div>
-      
+
       <Routes>
-        <Route path="/" element={<Home />}/> 
-        <Route path="/experience" element={<Experience/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
-  )
+  );
 }
